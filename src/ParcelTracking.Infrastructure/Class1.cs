6b865dd4bb1ce46a9527eb537ce46be898e0836c
MuhammedDotNet <muhammedmesutov@gmail.com>
2026-02-18 18:21:52 +0300

    Add Docker configuration for PostgreSQL and database context

--- src/ParcelTracking.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ParcelTracking.Infrastructure;
-
-public class Class1
-{
-
-}
---
+++ src/ParcelTracking.Infrastructure/Data/ParcelTrackingDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ParcelTracking.Domain.Entities;
+
+namespace ParcelTracking.Infrastructure.Data;
+
+public class ParcelTrackingDbContext : DbContext
+{
+    public ParcelTrackingDbContext(DbContextOptions<ParcelTrackingDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Parcel> Parcels => Set<Parcel>();
+    public DbSet<Address> Addresses => Set<Address>();
+    public DbSet<TrackingEvent> TrackingEvents => Set<TrackingEvent>();
+    public DbSet<ParcelContentItem> ParcelContentItems => Set<ParcelContentItem>();
+    public DbSet<DeliveryConfirmation> DeliveryConfirmations => Set<DeliveryConfirmation>();
+    public DbSet<ParcelWatcher> ParcelWatchers => Set<ParcelWatcher>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ParcelTrackingDbContext).Assembly);
+    }
+}
