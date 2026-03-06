e9ab177bb317096e2c71301ee534028369121c7b
MuhammedDotNet <muhammedmesutov@gmail.com>
2026-03-06 03:04:57 +0300

    feat: Add DeliveryTimeZoneId property to Parcel entity and update solution items.

--- src/ParcelTracking.Infrastructure/Migrations/20260305235837_AddDeliveryTimeZoneId.Designer.cs
+++ src/ParcelTracking.Infrastructure/Migrations/20260305235837_AddDeliveryTimeZoneId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ParcelTracking.Infrastructure.Data;
@@ -11,9 +12,11 @@ using ParcelTracking.Infrastructure.Data;
 namespace ParcelTracking.Infrastructure.Migrations
 {
     [DbContext(typeof(ParcelTrackingDbContext))]
-    partial class ParcelTrackingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260305235837_AddDeliveryTimeZoneId")]
+    partial class AddDeliveryTimeZoneId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ParcelTracking.Infrastructure/Migrations/20260305235837_AddDeliveryTimeZoneId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelTracking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryTimeZoneId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryTimeZoneId",
+                table: "Parcels",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryTimeZoneId",
+                table: "Parcels");
+        }
+    }
+}
